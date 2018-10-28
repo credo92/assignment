49eda966eee51d0d975f5692f9c5fa7779812321
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:47:51 -0400

    created histogram function in utils and returning plotlib object from function and using it in main.py to show histogram

--- src/main.py
+++ src/main.py
@@ -1,21 +1,15 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from utils import get_male_data
 from utils import get_female_data
+from utils import create_histogram
 
 def main():
     data = pd.read_csv("../data/data.csv") 
     
     male_age,male_height = get_male_data(data)
     female_age,female_height = get_female_data(data)
-    
-    #creating histogram
-    legend = ['Male', 'Female']
-    plt.hist([male_age, female_age], color=['Black', 'Red'], bins=20)
-    plt.xlabel(' Age Values')
-    plt.ylabel('Frequency')
-    plt.legend(legend)
-    plt.title('Male/Female Age Distribution')
+    plt = create_histogram(male_age, female_age)
     plt.show()
+    
     
 main()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def get_male_data(data):
     male_age =[]
     male_height= []
@@ -20,3 +22,13 @@ def get_female_data(data):
             female_height.append(row['height'])
     
     return female_age, female_height
+
+
+def create_histogram(male_data, female_data):
+    legend = ['Male', 'Female']
+    plt.hist([male_data, female_data], color=['Black', 'Red'], bins=20)
+    plt.xlabel('Age Values')
+    plt.ylabel('Frequency')
+    plt.legend(legend)
+    plt.title('Male/Female Age Distribution')
+    return plt
