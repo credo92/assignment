6228d54908d7cc49e5d63a15a9a68faa306493b0
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 14:00:31 -0400

    coverting data_dict to dataframe so we can extract female male lists

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -8,6 +8,10 @@ test_dict = {
               'age'    : pd.Series([32.0,36.0,35.0]),
               'height' : pd.Series([1.72,1.64,1.68])
              }
+# conver to df so we can extract female and male data lists from df
+# using util functins
+
+test_data = pd.DataFrame(test_dict)
 
 def test_get_male_data(): 
     assert 1==1
