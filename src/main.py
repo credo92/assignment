0c19d4dd4bf2047f1cd956e73c1ebb96d7bd9511
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:38:49 -0400

    creating histogram in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from utils import get_male_data
 from utils import get_female_data
@@ -7,8 +8,14 @@ def main():
     
     male_age,male_height = get_male_data(data)
     female_age,female_height = get_female_data(data)
-
-    print(female_age)   
-    print(female_height)
+    
+    #creating histogram
+    legend = ['Male', 'Female']
+    plt.hist([male_age, female_age], color=['Black', 'Red'], bins=50)
+    plt.xlabel(' Age Values')
+    plt.ylabel('Frequency')
+    plt.legend(legend)
+    plt.title('Male/Female Age Distribution')
+    plt.show()
     
 main()
