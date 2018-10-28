9d2c141719de0f98d4a0f612fd517a506b807bd6
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:12:19 -0400

    iterating over data and printing each row

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import get_split_line
+
 
 def main():
-    data = pd.read_csv("data/data.csv") 
-
+    data = pd.read_csv("../data/data.csv") 
+    
+    for index, row in data.iterrows():
+        print(row['gender'] , row['age'] , row['height'])
 main()
