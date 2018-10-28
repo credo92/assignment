7fab2f271303549287b4a4acbc7209a883d804a5
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:56:32 -0400

    adding test functionsin test_utils and importing all functions defined in utils.py

---
+++ src/tests/test_utils.py
@@ -0,0 +1,7 @@
+from src.utils import get_male_data
+from src.utils import get_female_data
+from src.utils import create_histogram
+
+def test_get_male_data(): 
+def test_get_female_data(): 
+def test_create_histogram_age_Distribution(): 
