0460a0fb60773df4c79b0a9d2c01605ae24aa6fe
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:14:15 -0400

    added if conditions and printing male and female rows

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def main():
     data = pd.read_csv("../data/data.csv") 
     
     for index, row in data.iterrows():
-        print(row['gender'] , row['age'] , row['height'])
+        if row['gender'] == 'M':
+            print('MALE ',row['gender'] , row['age'] , row['height'])    
+        if row['gender'] == 'F':    
+            print('FEMALE ', row['gender'] , row['age'] , row['height'])
 main()
