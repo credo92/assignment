bcc1c6d19b927d3eea2cccf789024712a9dd6206
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:58:40 -0400

    creating test data

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -2,6 +2,13 @@ from src.utils import get_male_data
 from src.utils import get_female_data
 from src.utils import create_histogram
 
+#test data
+test_dict = {
+              'gender'  : pd.Series(['M','F','F']),
+              'age'    : pd.Series([32.0,36.0,35.0]),
+              'height' : pd.Series([1.72,1.64,1.68])
+             }
+
 def test_get_male_data(): 
     assert 1==1
 def test_get_female_data(): 
