b30b7b1ab618d0214270b1801c9cbe1543df99c4
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:57:56 -0400

    adding sample assertions and running pytest src/tests/test_utill.py to see result

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -3,5 +3,8 @@ from src.utils import get_female_data
 from src.utils import create_histogram
 
 def test_get_male_data(): 
+    assert 1==1
 def test_get_female_data(): 
-def test_create_histogram_age_Distribution(): 
+    assert 1==1
+def test_create_histogram_age_Distribution():
+    assert 1==1
