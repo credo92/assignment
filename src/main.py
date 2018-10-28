743a552c2febe51d90d3835221879b4891756411
Vipul Srivastav <vipulsrivastav@MacBook-Pro-1575.local>
2018-10-28 13:16:46 -0400

    created separate lists to store male and female age/height values

--- src/main.py
+++ src/main.py
@@ -5,9 +5,20 @@ import matplotlib.pyplot as plt
 def main():
     data = pd.read_csv("../data/data.csv") 
     
+    male_age =[]
+    male_height= []
+    
+    female_age =[]
+    female_height= []
+    
     for index, row in data.iterrows():
         if row['gender'] == 'M':
-            print('MALE ',row['gender'] , row['age'] , row['height'])    
-        if row['gender'] == 'F':    
+            male_age.append(row['age'])
+            male_height.append(row['height'])
+            print('MALE ',row['gender'] , row['age'] , row['height'])
+            
+        if row['gender'] == 'F':
+            female_age.append(row['age'])
+            female_height.append(row['height'])
             print('FEMALE ', row['gender'] , row['age'] , row['height'])
 main()
